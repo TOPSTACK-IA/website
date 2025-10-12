00f86f10b3572486b401a7f8c7d00e2bae667067
TOPSTACK-BR <topstack1074@gmail.com>
2025-10-11 23:42:09 -0300

    update complet

--- push.bat
+++ push.bat
@@ -1,9 +1,21 @@
 @echo off
 cd /d %~dp0
 
+REM --- Informações do Repositório ---
+set REPO_URL=https://github.com/TOPSTACK-BR/website.git
+set GIT_USER_NAME="TOPSTACK-BR"
+set GIT_USER_EMAIL="topstack1074@gmail.com"
+
+
 echo.
-echo [ Git Push - topstack-site ]
+echo [ Git Config + Push - topstack-site ]
 echo ------------------------------------------
+
+git config user.name %GIT_USER_NAME%
+git config user.email %GIT_USER_EMAIL%
+
+git remote remove origin >nul 2>&1
+git remote add origin %REPO_URL%
 
 git add .
 
